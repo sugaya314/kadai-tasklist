11d0f7f664fc3a429592fa2cf9e0fbe319cba7d4
sugaya <sugaya314@outlook.jp>
2018-11-24 09:35:21 +0000

    kadai_columntuika

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,8 +3,10 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のタスク詳細ページ</h1>
+    
+    <p>ステータス: {{ $task->status }}</p>
 
-    <p>{{ $task->content }}</p>
+    <p>内容: {{ $task->content }}</p>
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
     
