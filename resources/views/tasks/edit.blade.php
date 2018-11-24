d87ffc73fb6778f7a2c03ab089d9c09998cdb5d7
sugaya <sugaya314@outlook.jp>
2018-11-24 13:20:59 +0000

    kadaibootstrap

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,17 +3,22 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-md-6">
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
+        <div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
+        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
+        <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
+        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
-
+        </div>
+    </div>
 @endsection
